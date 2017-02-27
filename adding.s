40c3acfd4819e3e2a4c914b45692cb0d061af903
bovojon <bovojon@gmail.com>
2017-02-27 00:03:15 -0500

    Add some values

--- adding.s
+++ adding.s
@@ -14,6 +14,10 @@
 my_main:
         # Main code goes here
 
+        li        $t1, 0x1111
+        li        $t2, 0x2222
+        add       $t3, $t1, $t2
+
 
         # Exit the program
         li        $v0, 10
