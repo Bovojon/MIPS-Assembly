5ebd0dd2df6f271d0aecb8d5155ada4a824001b0
bovojon <bovojon@gmail.com>
2017-02-26 23:51:04 -0500

    Build basic structure for MIPS program - 3 sections: data, text, exit

---
+++ adding.s
@@ -0,0 +1,14 @@
+# Demo Program
+# Filename: adding
+# Addition of Registers
+# Video lecture 4: MIPS #2: Add, Part 1
+
+        # Data Section
+        .data
+
+        # Text Section
+        .text
+
+        # Exit the program
+        li        $v0, 10
+        syscall
